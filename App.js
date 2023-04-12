7eda0aba00431fb0f5d5d410b9532b030b35611a
Ashwinnatarajan2001 <125949378+Ashwinnatarajan2001@users.noreply.github.com>
2023-04-11 23:13:15 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import"./App.css";
+import Board from './tic tok/tik tok';
+
+import Example from './from/example';
+class App extends Component {
+  render() {  
+  return (
+  <>
+
+  <Board/>
+  
+     </>
+  );
+}
+}        
+          
+export default App;
